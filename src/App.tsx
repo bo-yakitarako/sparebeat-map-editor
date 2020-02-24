f66c3334a633ce86eec518b27c7dc38d2e938e54
bo-yakitarako <admin@bo-yakitarako.com>
2020-02-24 13:14:57 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Classes, Button } from '@blueprintjs/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={Classes.DARK} style={{ width: "100%", height: "100vh"} }>
+            <div>
+                <Button icon="add" text="あほっしょしね" /><br/>
+                <a href="https://grade.sparebeat.bo-yakitarako.com" target="__blank">そこはかとなく尊い</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'normalize.css/normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
